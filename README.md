9db881ff8e4df63e413fe18f45e2c49c307e1047
xixirock <jlin0402@gmail.com>
2017-12-10 21:34:12 -0800

    Added array of words to guess and hangman

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Constructor_Hangman
+# This is a Hangman Game build using Constructor with Node.js. The theme is around Game's Title
---
+++ game.js
@@ -0,0 +1,5 @@
+//list of game's title to guess
+exports.newWord = {
+    wordList:['STARCRAFT','THE LAST OF US','WARFRAME','WORLD OF WARCRAFT','HEARTSTONE','LEAGUE OF LEGENDS','DESTINY','HALO','DOOM','METAL GEAR SOLID','CIVILIZATION','CRASH BANDICOOT','STREET FIGHTERS'],
+    hangman: ["\n\n\n\n\n\n\n-------------", "\n |\n |\n |\n |\n |\n |\n |\n-------------", "____________\n |         |\n |\n |\n |\n |\n |\n-------------","____________\n |         |\n |         O\n |\n |\n |\n |\n |\n-------------","____________\n |         |\n |         O\n |         |\n |         |\n |\n |\n |\n-------------","____________\n |         |\n |         O\n |         |\n |         |\n |        ---\n |\n |\n-------------", "____________\n |         |\n |         O\n |       __|\n |         |\n |        ---\n |\n |\n-------------", "____________\n |         |\n |         O\n |       __|__\n |         |\n |        ---\n |\n |\n-------------", "____________\n |         |\n |         O\n |       __|__\n |         |\n |        ---\n |        |\n |\n-------------","____________\n |         |\n |         O\n |       __|__\n |         |\n |        ---\n |        | |\n |\n-------------"]
+  };
