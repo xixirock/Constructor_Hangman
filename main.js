39ce7381c0355e88a91f2040b1f8d3e6c13510f2
xixirock <jlin0402@gmail.com>
2017-12-10 23:18:50 -0800

    initiate main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+//add in required
+var inquirer = require ('inquirer');
+var isLetter = require ('is-letter');
+var Word = require('./word.js');
+var Game = require('./game.js');
+
+var hangman = game.newWord.hangman;
