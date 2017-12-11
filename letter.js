6e365a4c08b6da73419f988c49e51cb4b9d9fb74
xixirock <jlin0402@gmail.com>
2017-12-10 21:47:31 -0800

    visit several ex, first funct only work if word bank item have no space, second func wrote

--- letter.js
+++ letter.js
@@ -9,8 +9,23 @@ var letter = function(let){
     this.letter = let;
     //Boolean value to determine whether the letter to be shown or not
     this.appear = false;
+    // works if word bank doesn't contain space, need other
+    // this.letterRender = function(){
+    //     return !(this.appear) ? "_" : this.letter;
+    // };
 
     this.letterRender = function(){
-        
-    }
-}
+        if(this.letter === ' '){
+            //allow function to read space as part of the string and not interpret as false
+            this.appear = true;
+            return ' ';
+        }
+
+        if(this.appear === false){
+            return '_';
+        }else{
+            return this.letter;
+        }
+    };
+};
+
